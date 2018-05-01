0f5576fe03e456e2390abd525a5debfe1d629465
Naimur Rahman Sourov <naimur.rahman103@gmail.com>
2018-05-01 12:41:16 +0600

    build file for heroku

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Hello this will be the site</h1>
+        <h1>Hello this will be the site!!!!!</h1>
       </div>
     );
   }
